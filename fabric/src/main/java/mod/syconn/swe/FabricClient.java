92f9e5ec6e14272f71e195bac5f8f428828c6b4a
Syconn <49461127+Syconn@users.noreply.github.com>
2024-08-07 23:56:52 -0400

    Fabric Fix

--- fabric/src/main/java/mod/syconn/swe/FabricClient.java
+++ fabric/src/main/java/mod/syconn/swe/FabricClient.java
@@ -1,16 +1,16 @@
 package mod.syconn.swe;
 
 import mod.syconn.swe.api.client.RenderUtil;
+import mod.syconn.swe.client.model.ChuteModel;
+import mod.syconn.swe.client.model.ParachuteModel;
+import mod.syconn.swe.client.model.TankModel;
 import mod.syconn.swe.client.renders.effects.MoonSpecialEffects;
 import mod.syconn.swe.client.renders.entity.layer.SpaceSuitLayer;
 import mod.syconn.swe.client.screen.gui.SpaceSuitOverlay;
 import mod.syconn.swe.init.ItemRegister;
 import mod.syconn.swe.items.Canister;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.DimensionRenderingRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.fabricmc.fabric.api.client.rendering.v1.LivingEntityFeatureRendererRegistrationCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.*;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.world.item.component.DyedItemColor;
@@ -26,7 +26,9 @@ public class FabricClient implements ClientModInitializer {
             if(entityRenderer instanceof PlayerRenderer renderer) registrationHelper.register(new SpaceSuitLayer<>(renderer, context.getModelSet()));
         });
         HudRenderCallback.EVENT.register(SpaceSuitOverlay.O2_OVERLAY::render);
-        Layer
+        EntityModelLayerRegistry.registerModelLayer(ParachuteModel.LAYER_LOCATION, ParachuteModel::createBodyLayer);
+        EntityModelLayerRegistry.registerModelLayer(ChuteModel.LAYER_LOCATION, ChuteModel::createBodyLayer);
+        EntityModelLayerRegistry.registerModelLayer(TankModel.LAYER_LOCATION, TankModel::createBodyLayer);
         DimensionRenderingRegistry.registerDimensionEffects(Constants.loc("moon"), new MoonSpecialEffects());
         ColorProviderRegistry.ITEM.register((s, layer) -> layer == 0 ? DyedItemColor.getOrDefault(s, -1) : -1, ItemRegister.PARACHUTE.get());
         ColorProviderRegistry.ITEM.register((s, layer) -> layer == 1  && getHandler(s) != null ? RenderUtil.getFluidColor(getHandler(s).getFluidInTank().fluid()) : -1, ItemRegister.CANISTER.get(), ItemRegister.AUTO_REFILL_CANISTER.get());
