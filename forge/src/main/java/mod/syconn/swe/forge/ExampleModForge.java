26cd2e07990a5a859835cbb7a08c20f7711fc60c
Syconn <49461127+Syconn@users.noreply.github.com>
2025-01-13 03:15:43 -0500

    Game Launches

--- forge/src/main/java/mod/syconn/swe/forge/ExampleModForge.java
+++ forge/src/main/java/mod/syconn/swe/forge/ExampleModForge.java
@@ -8,9 +8,8 @@ import mod.syconn.swe.ExampleMod;
 
 @Mod(ExampleMod.MOD_ID)
 public final class ExampleModForge {
-    public ExampleModForge() {
-        // Submit our event bus to let Architectury API register our content on the right time.
-        EventBuses.registerModEventBus(ExampleMod.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+    public ExampleModForge(FMLJavaModLoadingContext context) {
+        EventBuses.registerModEventBus(ExampleMod.MOD_ID, context.getModEventBus());
 
         // Run our common setup.
         ExampleMod.init();
