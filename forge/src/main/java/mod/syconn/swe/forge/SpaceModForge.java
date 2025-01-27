8fd95ae55072d1dac1e7135d20541940df45f479
Syconn <49461127+Syconn@users.noreply.github.com>
2025-01-27 15:25:46 -0500

    Forge Fix

--- forge/src/main/java/mod/syconn/swe/forge/SpaceModForge.java
+++ forge/src/main/java/mod/syconn/swe/forge/SpaceModForge.java
@@ -1,14 +1,16 @@
 package mod.syconn.swe.forge;
 
 import dev.architectury.platform.forge.EventBuses;
+import mod.syconn.swe.Constants;
+import mod.syconn.swe.SpaceMod;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(ExampleMod.MOD_ID)
+@Mod(Constants.MOD)
 public final class SpaceModForge {
 
-    public SpaceModForge(FMLJavaModLoadingContext context) {
-        EventBuses.registerModEventBus(ExampleMod.MOD_ID, context.getModEventBus());
-        ExampleMod.init();
+    public SpaceModForge() {
+        EventBuses.registerModEventBus(Constants.MOD, FMLJavaModLoadingContext.get().getModEventBus());
+        SpaceMod.init();
     }
 }
